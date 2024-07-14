a90d577ed0de34e3988ea3a3627bbc56b32d5dc1
navaneethsujith09 <43727786+navaneethsujith09@users.noreply.github.com>
2024-07-14 08:03:01 +0530

    Update exo5.js

--- Exercise-5/exo5.js
+++ Exercise-5/exo5.js
@@ -1,27 +1,41 @@
 function formValidator(firstName, lastName, age, phoneNumber) {
-  const userInfo = {firstName, lastName, age, phoneNumber};
-  const errorMessages = [];
-  for (const key in userInfo) {
-    if (typeof userInfo[key] === 'undefined') {
-      errorMessages.push(`The ${key} input is missing.`);
-    }
+  if (typeof firstName === 'undefined') {
+    return "The name input is missing.";
   }
-  for (const key in userInfo) {
-    if (typeof userInfo[key] !== (key === 'age' ? 'number' : 'string')) {
-      errorMessages.push(`The ${key} should be a ${key === 'age' ? 'number' : 'string'}, but received ${typeof userInfo[key]}.`);
-    }
+  if (typeof lastName === 'undefined') {
+    return "The last name input is missing.";
   }
-  if (userInfo.age < 18) {
-    errorMessages.push("Sorry, not old enough for our app.");
+  if (typeof age === 'undefined') {
+    return "The age input is missing.";
   }
-  if (errorMessages.length > 0) {
-    return errorMessages.join('\n');
-  } else {
-    return "WELCOME TO THE ADOS APP.";
+  if (typeof phoneNumber === 'undefined') {
+    return "The phone number input is missing.";
   }
+
+  if (typeof firstName !== 'string') {
+    return `The first name should be a string, but received ${typeof firstName}.`;
+  }
+  if (typeof lastName !== 'string') {
+    return `The last name should be a string, but received ${typeof lastName}.`;
+  }
+  if (typeof phoneNumber !== 'string') {
+    return `The phone number should be a string, but received ${typeof phoneNumber}.`;
+  }
+  if (typeof age !== 'number') {
+    return "The age should be a number.";
+  }
+
+  if (age < 18) {
+    return "Sorry, not old enough for our app.";
+  }
+
+
+  return "WELCOME TO THE ADOS APP.";
 }
 //TEST
 console.log(formValidator('John', 'Doe', 25, '1234567890'));
 console.log(formValidator('John', 'Doe', 'twentyfive', '1234567890'));
 console.log(formValidator('John', 'Doe', 17, '1234567890'));
 console.log(formValidator('John', 'Doe', 25, '123456789'));
+
+
