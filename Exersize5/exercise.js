132d9c908e1d008929290d64a36edee00ea99e1d
Consolegamer2 <160079707+Consolegamer2@users.noreply.github.com>
2025-07-12 13:48:47 -0400

    Finished exercise

--- Exersize5/exercise.js
+++ Exersize5/exercise.js
@@ -1,31 +1,49 @@
-function formValidator(fName, lName, age, pNumber){
+function formValidator(fName, lName, age, pNumber) {
      if (fName === null || fName.length === 0) {
-        console.log("The first name input is missing");
+        return "The first name input is missing";  
     }
+
     else if (lName === null || lName.length === 0) {
-        console.log("The last name input is missing");
+        return "The last name input is missing";
     }
+
     else if(age === null) {
-        console.log("The age input is missing");
+        return "The age input is missing";
     }
+
     else if (pNumber === null || pNumber.length === 0) {
-        console.log("The phone number is missing");
+        return "The phone number is missing";
     }
 
 
     if (typeof fName !== 'string') {
-        console.log("first Name should be a string");
+        return "first Name should be a string";
     }
+
     else if (typeof lName !== 'string') {
-        console.log("lastt Name should be a string");
+        return "lastt Name should be a string";
     }
+
     else if (!Number.isInteger(age)) {
-        console.log("Age should be a number");
+        return "Age should be a number";
     }
+    
     else if (typeof pNumber !== 'string') {
-        console.log("Phone Number should be a string");
+        return "Phone Number should be a string";
     }
+
+    if(age < 18) {
+        return "Sorry, not old enough for our App";
+
+    }
+
+    return "Welcome to the ADOS App";
 
 
 
 }
+// TESTING
+console.log(formValidator("John", "Doe", 18, "555-555-5555"));
+console.log(formValidator("John", "Doe", 17, 55));
+console.log(formValidator("", "Doe", 18, "555-555-5555"));
+console.log(formValidator("John", "" ,18, "555-555-5555"));
