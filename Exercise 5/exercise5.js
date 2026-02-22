08df6f004a16752272b7f04b352d307bc9e77b70
Vybhav26 <vybhav.28.parthan@gmail.com>
2026-02-22 10:32:04 -0500

    Exercise 5 Done

--- Exercise 5/exercise5.js
+++ Exercise 5/exercise5.js
@@ -1,30 +1,22 @@
-
-
 function formValidator(firstName, lastName, age, phoneNumber) {
 
     switch(true){
 
-        case (firstName == undefined || firstName == "" || firstName == null):
+        case (firstName === undefined || firstName === "" || firstName === null):
+            console.log("First Name field is missing/invalid");
+            return;
         
-        console.log("First Name field is missing/invalid");
-        return;
-        
-        case (lastName == undefined || lastName == "" || lastName == null):
-        
-        console.log("Last Name field is missing/invalid");
-        return;
+        case (lastName === undefined || lastName === "" || lastName === null):
+            console.log("Last Name field is missing/invalid");
+            return;
 
-
-        case (age == undefined || age== "" || age == null):
+        case (age === undefined || age === "" || age === null):
+            console.log("Age field is missing/invalid");
+            return;
         
-        console.log("Age field is missing/invalid");
-        return;
-        
-        case (phoneNumber == undefined || phoneNumber == "" || phoneNumber == null):
-        
-        console.log("Age field is missing/invalid");
-        return;
-
+        case (phoneNumber === undefined || phoneNumber === "" || phoneNumber === null):
+            console.log("Phone Number field is missing/invalid");
+            return;
 
         case (typeof firstName !== 'string'):
             console.log("First name should be a string.");
@@ -46,12 +38,9 @@ function formValidator(firstName, lastName, age, phoneNumber) {
             console.log("Sorry, not old enough for our app.");
             return;
 
-
         default: 
-        console.log("WELCOME TO THE ADOS APP.");
-
+            console.log("WELCOME TO THE ADOS APP.");
     }
-
 }
 
-formValidator("V", "P", 19, "123-456-7890")
+formValidator("V", "P", 19, "123-456-7890");
