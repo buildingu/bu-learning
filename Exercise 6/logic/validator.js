f91d7ae6e3dcec67d3e086b12df95dd7eb877de1
cayla-hhh <cayla.li.huang@gmail.com>
2026-04-18 07:13:22 -0700

    Add navigation bar and connect page to logic page

--- Exercise 6/logic/validator.js
+++ Exercise 6/logic/validator.js
@@ -15,7 +15,7 @@ function formValidator(firstName, lastName, age, phoneNumber) {
     if (!user.lastName || user.lastName.trim() === "") {
         errors.push("The last name is missing or blank.");
     }
-    if (user.age === "" || user.age === undefined || user.age === null) {
+    if (isNaN(user.age) || user.age === undefined || user.age === null) {
         errors.push("The age is missing.");
     }
     if (!user.phoneNumber || user.phoneNumber.trim() === "") {
@@ -37,13 +37,13 @@ function formValidator(firstName, lastName, age, phoneNumber) {
     return errors;
 }
 
-document.getElementById('submitBtn').addEventListener('click', function() {
+document.getElementById('v-submit').addEventListener('click', function() {
     const msgBox = document.getElementById('messageBox');
     
-    const fName = document.getElementById('firstName').value;
-    const lName = document.getElementById('lastName').value;
-    const age = parseInt(document.getElementById('age').value); 
-    const phone = document.getElementById('phoneNumber').value;
+    const fName = document.getElementById('v-firstName').value;
+    const lName = document.getElementById('v-lastName').value;
+    const age = parseInt(document.getElementById('v-age').value); 
+    const phone = document.getElementById('v-phoneNumber').value;
 
     const errors = formValidator(fName, lName, age, phone);
 
