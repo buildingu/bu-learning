de390183bd27391201dcd102455a3c8983024675
queenjuhithestar1-source <queenjuhithestar1@gmail.com>
2026-07-02 10:43:39 -0500

    Fixed lesson 4 accordingly to the feedback I have recieved

--- React_Lesson_Exercise-master/React_Lesson_Exercise-master/src/components/4.UseContextChallenge/UseContext.jsx
+++ React_Lesson_Exercise-master/React_Lesson_Exercise-master/src/components/4.UseContextChallenge/UseContext.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { AuthContext } from "./AuthContext";
+import AuthContext from "./AuthContext";
 
 export default function UseContextChallenge() {
   const { user, login, logout } = useContext(AuthContext);
